04bcb198059d937561ad3c139a3e7136b347f23a
Benjamin Heydorn <benjamin.heydorn@gmail.com>
2021-10-13 14:51:40 -0600

    some changes

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -4,14 +4,22 @@ from game.word import Word
 
 class Director:
     def __init__(self):
-        pass
+        self.parachute = Parachute()
+        self.console = Console()
+        self.word = Word()
+        self.gameover = False
     
     def start_game(self):
-        pass
+        while not self.gameover:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
 
     def get_inputs(self):
         # interacts with console class
-        pass
+        letter_guess = self.console.read("Guess a letter [a-z]: ")
+        
+
 
     def do_updates(self):
         # updates game information (word, parachute)
