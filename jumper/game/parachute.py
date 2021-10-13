b973cc2df3c1fd59dd199f64e7e8c9f93551fad6
Samuel-Benson <samuelgbens@gmail.com>
2021-10-13 14:51:48 -0600

    Update parachute.py

--- jumper/game/parachute.py
+++ jumper/game/parachute.py
@@ -1,12 +1,14 @@
-def display_chute():
-    chute = ["  ___  "," /___\\ "," \\   / ","  \\ /","   0   ","  /|\\   ","  / \\   ","^^^^^^^"]
-    print(chute[0])
-    print(chute[1])
-    print(chute[2])
-    print(chute[3])
-    print(chute[4])
-    print(chute[5])
-    print(chute[6])
-    print(chute[7])
-    return
-display_chute()
+class Parachute():
+
+    def __init__():
+        chute = ["  ___  "," /___\\ "," \\   / ","  \\ /","   0   ","  /|\\   ","  / \\   ","^^^^^^^"]
+        print(chute[0])
+        print(chute[1])
+        print(chute[2])
+        print(chute[3])
+        print(chute[4])
+        print(chute[5])
+        print(chute[6])
+        print(chute[7])
+        return
+    display_chute()
