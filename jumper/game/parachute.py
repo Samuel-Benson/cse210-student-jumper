be2e68b32e9b9c6d810ffe80e0475f09731155cc
Samuel-Benson <samuelgbens@gmail.com>
2021-10-14 14:59:46 -0600

    Update parachute.py

--- jumper/game/parachute.py
+++ jumper/game/parachute.py
@@ -17,19 +17,18 @@ class Parachute():
         # makes parachute lose one piece at top
         # SPECIAL CASE: When the last parachute piece is cut, change the stick man's
         # head from a zero ( "   0   " ) to an x ( "   x   " )
-        self.chute[0] = ""
-        if self.chute[0] == "":
-            self.chute[1] = ""
-        elif self.chute[0] and self.chute [1] == "":
-            self.chute[2] = ""
-        elif self.chute[0] and self.chute [1] and self.chute[2] == "":
-            self.chute[3] = ""
+        chute.pop(0)
+        if chute.pop(0) = "   0   ":
+            chute[0] = "   X   "
         pass
 
     def is_game_over(self):
         # Return true if the parachute is dead, false it is is not
+        if chute[0] = "   X   ":
+            return True
         pass
 
     def generate_output(self):
         # return a string of the parachute to be printed
+        chute = "  ___  \n /___\\ \n \\   / \n  \\ /\n   0   \n  /|\\   \n  / \\   \n^^^^^^^"
         pass
