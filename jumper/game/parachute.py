e5ac89588e69d06b3ee6d7f197d375872090bb03
Samuel-Benson <samuelgbens@gmail.com>
2021-10-15 13:32:26 -0600

    Update parachute.py

--- jumper/game/parachute.py
+++ jumper/game/parachute.py
@@ -1,7 +1,7 @@
 class Parachute():
 
     def __init__(self):
-        self.chute = ["  ___  "," /___\\ "," \\   / ","  \\ /","   0   ","  /|\\   ","  / \\   ","^^^^^^^"]
+        chute = ["  ___  "," /___\\ "," \\   / ","  \\ /","   0   ","  /|\\   ","  / \\   ","^^^^^^^"]
         # Change these print statements into a parachute attribute. (a list)
         return
 
@@ -10,13 +10,13 @@ class Parachute():
         # SPECIAL CASE: When the last parachute piece is cut, change the stick man's
         # head from a zero ( "   0   " ) to an x ( "   x   " )
         chute.pop(0)
-        if chute.pop(0) = "   0   ":
+        if chute.pop(0) == "   0   ":
             chute[0] = "   X   "
         pass
 
     def is_game_over(self):
         # Return true if the parachute is dead, false it is is not
-        if chute[0] = "   X   ":
+        if chute[0] == "   X   ":
             return True
         pass
 
